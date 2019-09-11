f3fa7b324275cd4eb89968644a90ef4c52843159
pinux <1063440021@qq.com>
2019-09-11 17:22:09 +0800

    first commit

---
+++ src/main/java/com/pinux/springbootlogback/OtherController.java
@@ -0,0 +1,45 @@
+package com.pinux.springbootlogback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * @ClassName TestController
+ * @Description TODO
+ * @Author pinux
+ * @Date 2019/9/11 15:23
+ * @Version 1.0
+ */
+@RestController
+public class OtherController {
+    private Logger logger= LoggerFactory.getLogger("other");
+
+    @RequestMapping(value = "/other")
+    public String test(){
+        logger.debug("Test->debug");
+        logger.info("Test->info");
+        logger.warn("Test->warn");
+        logger.error("Test->error");
+        return "test";
+    }
+}
---
+++ src/main/java/com/pinux/springbootlogback/controller/IndexController.java
@@ -0,0 +1,49 @@
+package com.pinux.springbootlogback.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * @ClassName TestController
+ * @Description TODO
+ * @Author pinux
+ * @Date 2019/9/11 15:00
+ * @Version 1.0
+ */
+@RestController
+public class IndexController {
+
+    private Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+
+    @RequestMapping(value = "/index")
+    public String index() {
+        logger.debug("debug");
+        logger.info("info");
+        logger.error("error");
+        return "index";
+
+    }
+
+
+}
